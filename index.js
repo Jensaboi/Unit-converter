fd0c802d65e212c001aef813ec2bcabc9819bd44
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-06-16 02:10:55 +0200

    js logic
    finished js logic

---
+++ index.js
@@ -0,0 +1,53 @@
+//Round down to max 3 decimals
+
+const FEET = 3.281; // 1 meter = 3.281 feet
+const GALLON = 0.264; // 1 liter = 0.264 gallon
+const POUND = 2.204; // 1 kilogram = 2.204 pound
+
+const numberInputEl = document.getElementById("number-input");
+const containerEl = document.getElementById("container-el");
+
+function renderElements() {
+    const input = parseFloat(numberInputEl.value);
+    const data = calculateMetrics(input);
+    let domElements = "";
+
+    data.forEach((item) => {
+        domElements += `
+        <div>
+            <h3>${item.title}</h3>
+            <p>${item.value}</p>
+        </div>
+        `;
+    });
+    containerEl.innerHTML = domElements;
+}
+// returns array with objects. title: string(metric type), value: string(calculation)
+function calculateMetrics(num) {
+    if (isNaN(num)) return [];
+    return [
+        {
+            title: "Length (Meter/Feet)",
+            value: `${num} meters = ${(num * FEET).toFixed(
+                3
+            )} feet | ${num} feet = ${(num / FEET).toFixed(3)} meters`,
+        },
+        {
+            title: "Volume (Liters/Gallons)",
+            value: `${num} liters = ${(num * GALLON).toFixed(
+                3
+            )} gallons | ${num} gallons = ${(num / GALLON).toFixed(3)} liters `,
+        },
+        {
+            title: "Mass (Kilograms/Pounds)",
+            value: `${num} kilo = ${(num * POUND).toFixed(
+                3
+            )} pounds | ${num} pounds = ${(num / POUND).toFixed(3)} kilos`,
+        },
+    ];
+}
+
+//Initializing btn
+document.getElementById("convert-btn").addEventListener("click", () => {
+    renderElements();
+});
