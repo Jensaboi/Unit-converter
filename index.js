1d09be875e8dca1fa68bc9bf0c7004fe863332c1
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2025-06-16 11:46:23 +0200

    added darkmode and styles
    Added darkmode state thats stored in localstorage and styles in index.css

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 //Round down to max 3 decimals
-
 const FEET = 3.281; // 1 meter = 3.281 feet
 const GALLON = 0.264; // 1 liter = 0.264 gallon
 const POUND = 2.204; // 1 kilogram = 2.204 pound
 
 const numberInputEl = document.getElementById("number-input");
 const containerEl = document.getElementById("container-el");
+
+let isDarkMode = JSON.parse(localStorage.getItem("darkmode") || "false");
+
+applyThemeSettings(isDarkMode);
+
+function applyThemeSettings(theme) {
+    document.body.classList.toggle("dark-mode", theme);
+}
+
+function toggleDarkMode() {
+    isDarkMode = !isDarkMode;
+    localStorage.setItem("darkmode", JSON.stringify(isDarkMode));
+
+    document.body.classList.toggle("dark-mode", isDarkMode);
+    if (isDarkMode) {
+        document.getElementById("dark-mode-btn").textContent = "🌝";
+    } else {
+        document.getElementById("dark-mode-btn").textContent = "🌚";
+    }
+}
 
 function renderElements() {
     const input = parseFloat(numberInputEl.value);
@@ -14,7 +33,7 @@ function renderElements() {
 
     data.forEach((item) => {
         domElements += `
-        <div>
+        <div class="result-card">
             <h3>${item.title}</h3>
             <p>${item.value}</p>
         </div>
@@ -22,6 +41,7 @@ function renderElements() {
     });
     containerEl.innerHTML = domElements;
 }
+
 // returns array with objects. title: string(metric type), value: string(calculation)
 function calculateMetrics(num) {
     if (isNaN(num)) return [];
@@ -50,4 +70,8 @@ function calculateMetrics(num) {
 //Initializing btn
 document.getElementById("convert-btn").addEventListener("click", () => {
     renderElements();
+});
+
+document.getElementById("dark-mode-btn").addEventListener("click", () => {
+    toggleDarkMode();
 });
